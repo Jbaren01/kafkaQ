53777e26d13ab2f9ad6391695f76b3d913b67a79
Jonathan Barenboim <dontpanicproductions@Jonathans-iMac.local>
2020-10-23 17:18:24 -0700

    added instrumentation event CURRENTLY WORKING

--- kafkaApplicationSimulator/consumer.js
+++ kafkaApplicationSimulator/consumer.js
@@ -1,40 +1,53 @@
 const { Kafka } = require('kafkajs');
-const { REQUEST_QUEUE_SIZE } = consumer.events;
-const removeListener = consumer.on(REQUEST_QUEUE_SIZE, (e) =>
-  console.log('this is queue size', e.queueSize)
-);
+
+// const { REQUEST_QUEUE_SIZE } = consumer.events;
+// const removeListener = consumer.on(REQUEST_QUEUE_SIZE, (e) =>
+//   console.log('this is queue size', e.queueSize)
+// );
 // const msg = process.argv[2];
 
 run();
 async function run() {
   try {
-    const kafka = new Kafka({
-      clientId: 'myapp',
-      brokers: ['Jonathans-iMac.local:9092'],
-    });
-    const consumer = kafka.consumer({
-      groupId: 'test',
-    });
-    console.log('connecting...');
-    await consumer.connect();
-    console.log('connected!');
+        const kafka = new Kafka({
+          clientId: 'myapp',
+          brokers: ['Jonathans-iMac.local:9092'],
+        });
+        const consumer = kafka.consumer({
+          groupId: 'test',
+        });
 
-    await consumer.subscribe({
-      topic: 'Users',
-      fromBeginning: true,
-    });
-
-    await consumer.run({
-      eachMessage: async (result) => {
-        console.log(
-          `MSG: ${result.message.value} PARTITION: ${result.partition}`
+        // Instrumentation Event to monitor consumer heartbeat
+        const { HEARTBEAT } = consumer.events;
+        const removeListener = consumer.on(HEARTBEAT, (e) =>
+          console.log(`heartbeat -----------> at ${e.payload.groupId}`)
         );
-      },
-    });
-    console.log('before queue size');
-    removeListener();
-  } catch (err) {
+        console.log(removeListener);
+
+        console.log('connecting...');
+        await consumer.connect();
+        console.log('connected!');
+
+        await consumer.subscribe({
+          topic: 'Users',
+          fromBeginning: true,
+        });
+
+        // const { REQUEST_QUEUE_SIZE } = consumer.events;
+        // const removeListener = consumer.on(REQUEST_QUEUE_SIZE, (e) =>
+        //   console.log(`queue size -----------> at ${e.timestamp}`)
+        // );
+        // console.log(removeListener);
+
+        await consumer.run({
+          eachMessage: async (result) => {
+            console.log(
+              `MSG: ${result.message.value} PARTITION: ${result.partition}`
+            );
+          },
+        });
+      } catch (err) {
     console.log(`ERROR: ${err}`);
   } finally {
-  }
+            }
 }
--- kafkaApplicationSimulator/controllers/kafkaController.js
+++ kafkaApplicationSimulator/controllers/kafkaController.js
@@ -1,39 +1,69 @@
+const { EventEmitter } = require('events');
 const { Kafka } = require('kafkajs');
+// const { HEARTBEAT } = consumer.events;
+
 
 const kafkaController = {};
 
 kafkaController.produceMessage = async (req, res, next) => {
-  console.log('hi');
-  try {
-    const kafka = new Kafka({
-      clientId: 'myapp',
-      brokers: ['Jonathans-iMac.local:9092'],
-    });
-    const producer = kafka.producer();
-    console.log('connecting...');
-    await producer.connect();
-    console.log('connected!');
+                                                             // const removeListener = consumer.on(HEARTBEAT, (e) =>
+                                                             //   console.log(`heartbeat at ${e.timestamp}`)
+                                                             // );
 
-    const { msg } = req.body;
-    console.log(msg);
+                                                             try {
+                                                               const kafka = new Kafka(
+                                                                 {
+                                                                   clientId:
+                                                                     'myapp',
+                                                                   brokers: [
+                                                                     'Jonathans-iMac.local:9092',
+                                                                   ],
+                                                                 }
+                                                               );
+                                                               const producer = kafka.producer();
+                                                               console.log(
+                                                                 'connecting...'
+                                                               );
+                                                               await producer.connect();
+                                                               console.log(
+                                                                 'connected!'
+                                                               );
 
-    const partition = msg[0].toLowerCase() < 'n' ? 0 : 1;
-    const result = await producer.send({
-      topic: 'Users',
-      messages: [
-        {
-          value: msg,
-          partition: partition,
-        },
-      ],
-    });
-    console.log(`send successfully! ${JSON.stringify(result)}`);
-    await producer.disconnect();
-  } catch (err) {
-    console.error(`ERROR: ${err}`);
-  } finally {
-    next();
-  }
-};
+                                                               const {
+                                                                 msg,
+                                                               } = req.body;
+                                                               console.log(msg);
+
+                                                               const partition =
+                                                                 msg[0].toLowerCase() <
+                                                                 'n'
+                                                                   ? 0
+                                                                   : 1;
+                                                               const result = await producer.send(
+                                                                 {
+                                                                   topic:
+                                                                     'Users',
+                                                                   messages: [
+                                                                     {
+                                                                       value: msg,
+                                                                       partition: partition,
+                                                                     },
+                                                                   ],
+                                                                 }
+                                                               );
+                                                               console.log(
+                                                                 `send successfully! ${JSON.stringify(
+                                                                   result
+                                                                 )}`
+                                                               );
+                                                               await producer.disconnect();
+                                                             } catch (err) {
+                                                               console.error(
+                                                                 `ERROR: ${err}`
+                                                               );
+                                                             } finally {
+                                                               next();
+                                                             }
+                                                           };
 
 module.exports = kafkaController;
