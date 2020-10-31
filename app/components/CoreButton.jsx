0a7940bc3c4b07fa46b0186a74c1c1a870fd2624
Jonathan Barenboim <Jonathan.Barenboim@gmail.com>
2020-10-31 08:45:46 -0700

    kaiwei said delete git file

---
+++ app/components/CoreButton.jsx
@@ -0,0 +1,390 @@
+import React from 'react';
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+} from '@coreui/react';
+
+const BrandButtons = () => {
+  return (
+    <CRow>
+      <CCol xs="12">
+        <CCard>
+          <CCardHeader>Brand button</CCardHeader>
+          <CCardBody>
+            <h6>
+              Size Small
+              <small>
+                {' '}
+                Add this class <code>.btn-sm</code>
+              </small>
+            </h6>
+            <p>
+              <CButton size="sm" className="btn-facebook btn-brand mr-1 mb-1">
+                <span>Facebook</span>
+              </CButton>
+              <CButton size="sm" className="btn-twitter btn-brand mr-1 mb-1">
+                <span>Twitter</span>
+              </CButton>
+              <CButton size="sm" className="btn-linkedin btn-brand mr-1 mb-1">
+                <span>LinkedIn</span>
+              </CButton>
+              <CButton size="sm" className="btn-flickr btn-brand mr-1 mb-1">
+                <span>Flickr</span>
+              </CButton>
+              <CButton size="sm" className="btn-tumblr btn-brand mr-1 mb-1">
+                <span>Tumblr</span>
+              </CButton>
+              <CButton size="sm" className="btn-xing btn-brand mr-1 mb-1">
+                <span>Xing</span>
+              </CButton>
+              <CButton size="sm" className="btn-github btn-brand mr-1 mb-1">
+                <span>Github</span>
+              </CButton>
+              <CButton
+                size="sm"
+                className="btn-stack-overflow btn-brand mr-1 mb-1"
+              >
+                <span>StackOverflow</span>
+              </CButton>
+              <CButton size="sm" className="btn-youtube btn-brand mr-1 mb-1">
+                <span>YouTube</span>
+              </CButton>
+              <CButton size="sm" className="btn-dribbble btn-brand mr-1 mb-1">
+                <span>Dribbble</span>
+              </CButton>
+              <CButton size="sm" className="btn-instagram btn-brand mr-1 mb-1">
+                <span>Instagram</span>
+              </CButton>
+              <CButton size="sm" className="btn-pinterest btn-brand mr-1 mb-1">
+                <span>Pinterest</span>
+              </CButton>
+              <CButton size="sm" className="btn-vk btn-brand mr-1 mb-1">
+                <span>VK</span>
+              </CButton>
+              <CButton size="sm" className="btn-yahoo btn-brand mr-1 mb-1">
+                <span>Yahoo</span>
+              </CButton>
+              <CButton size="sm" className="btn-behance btn-brand mr-1 mb-1">
+                <span>Behance</span>
+              </CButton>
+              <CButton size="sm" className="btn-reddit btn-brand mr-1 mb-1">
+                <span>Reddit</span>
+              </CButton>
+              <CButton size="sm" className="btn-vimeo btn-brand mr-1 mb-1">
+                <span>Vimeo</span>
+              </CButton>
+            </p>
+            <h6>Size Normal</h6>
+            <p>
+              <CButton className="btn-facebook btn-brand mr-1 mb-1">
+                <span>Facebook</span>
+              </CButton>
+              <CButton className="btn-twitter btn-brand mr-1 mb-1">
+                <span>Twitter</span>
+              </CButton>
+              <CButton className="btn-linkedin btn-brand mr-1 mb-1">
+                <span>LinkedIn</span>
+              </CButton>
+              <CButton className="btn-flickr btn-brand mr-1 mb-1">
+                <span>Flickr</span>
+              </CButton>
+              <CButton className="btn-tumblr btn-brand mr-1 mb-1">
+                <span>Tumblr</span>
+              </CButton>
+              <CButton className="btn-xing btn-brand mr-1 mb-1">
+                <span>Xing</span>
+              </CButton>
+              <CButton className="btn-github btn-brand mr-1 mb-1">
+                <span>Github</span>
+              </CButton>
+              <CButton className="btn-stack-overflow btn-brand mr-1 mb-1">
+                <span>StackOverflow</span>
+              </CButton>
+              <CButton className="btn-youtube btn-brand mr-1 mb-1">
+                <span>YouTube</span>
+              </CButton>
+              <CButton className="btn-dribbble btn-brand mr-1 mb-1">
+                <span>Dribbble</span>
+              </CButton>
+              <CButton className="btn-instagram btn-brand mr-1 mb-1">
+                <span>Instagram</span>
+              </CButton>
+              <CButton className="btn-pinterest btn-brand mr-1 mb-1">
+                <span>Pinterest</span>
+              </CButton>
+              <CButton className="btn-vk btn-brand mr-1 mb-1">
+                <span>VK</span>
+              </CButton>
+              <CButton className="btn-yahoo btn-brand mr-1 mb-1">
+                <span>Yahoo</span>
+              </CButton>
+              <CButton className="btn-behance btn-brand mr-1 mb-1">
+                <span>Behance</span>
+              </CButton>
+              <CButton className="btn-reddit btn-brand mr-1 mb-1">
+                <span>Reddit</span>
+              </CButton>
+              <CButton className="btn-vimeo btn-brand mr-1 mb-1">
+                <span>Vimeo</span>
+              </CButton>
+            </p>
+            <h6>
+              Size Large
+              <small>
+                {' '}
+                Add this class <code>.btn-lg</code>
+              </small>
+            </h6>
+            <p>
+              <CButton size="lg" className="btn-facebook btn-brand mr-1 mb-1">
+                <span>Facebook</span>
+              </CButton>
+              <CButton size="lg" className="btn-twitter btn-brand mr-1 mb-1">
+                <span>Twitter</span>
+              </CButton>
+              <CButton size="lg" className="btn-linkedin btn-brand mr-1 mb-1">
+                <span>LinkedIn</span>
+              </CButton>
+              <CButton size="lg" className="btn-flickr btn-brand mr-1 mb-1">
+                <span>Flickr</span>
+              </CButton>
+              <CButton size="lg" className="btn-tumblr btn-brand mr-1 mb-1">
+                <span>Tumblr</span>
+              </CButton>
+              <CButton size="lg" className="btn-xing btn-brand mr-1 mb-1">
+                <span>Xing</span>
+              </CButton>
+              <CButton size="lg" className="btn-github btn-brand mr-1 mb-1">
+                <span>Github</span>
+              </CButton>
+              <CButton
+                size="lg"
+                className="btn-stack-overflow btn-brand mr-1 mb-1"
+              >
+                <span>StackOverflow</span>
+              </CButton>
+              <CButton size="lg" className="btn-youtube btn-brand mr-1 mb-1">
+                <span>YouTube</span>
+              </CButton>
+              <CButton size="lg" className="btn-dribbble btn-brand mr-1 mb-1">
+                <span>Dribbble</span>
+              </CButton>
+              <CButton size="lg" className="btn-instagram btn-brand mr-1 mb-1">
+                <span>Instagram</span>
+              </CButton>
+              <CButton size="lg" className="btn-pinterest btn-brand mr-1 mb-1">
+                <span>Pinterest</span>
+              </CButton>
+              <CButton size="lg" className="btn-vk btn-brand mr-1 mb-1">
+                <span>VK</span>
+              </CButton>
+              <CButton size="lg" className="btn-yahoo btn-brand mr-1 mb-1">
+                <span>Yahoo</span>
+              </CButton>
+              <CButton size="lg" className="btn-behance btn-brand mr-1 mb-1">
+                <span>Behance</span>
+              </CButton>
+              <CButton size="lg" className="btn-reddit btn-brand mr-1 mb-1">
+                <span>Reddit</span>
+              </CButton>
+              <CButton size="lg" className="btn-vimeo btn-brand mr-1 mb-1">
+                <span>Vimeo</span>
+              </CButton>
+            </p>
+          </CCardBody>
+        </CCard>
+      </CCol>
+
+      <CCol xs="12">
+        <CCard>
+          <CCardHeader>
+            Brand button
+            <small> only icons</small>
+          </CCardHeader>
+          <CCardBody>
+            <h6>
+              Size Small
+              <small>
+                {' '}
+                Add this class <code>.btn-sm</code>
+              </small>
+            </h6>
+            <p>
+              <CButton
+                size="sm"
+                className="btn-facebook btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-twitter btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-linkedin btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-flickr btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-tumblr btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-xing btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-github btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-stack-overflow btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-youtube btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-dribbble btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-instagram btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-pinterest btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-vk btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-yahoo btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-behance btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-reddit btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="sm"
+                className="btn-vimeo btn-brand mr-1 mb-1"
+              ></CButton>
+            </p>
+            <h6>Size Normal</h6>
+            <p>
+              <CButton className="btn-facebook btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-twitter btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-linkedin btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-flickr btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-tumblr btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-xing btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-github btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-stack-overflow btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-youtube btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-dribbble btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-instagram btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-pinterest btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-vk btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-yahoo btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-behance btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-reddit btn-brand mr-1 mb-1"></CButton>
+              <CButton className="btn-vimeo btn-brand mr-1 mb-1"></CButton>
+            </p>
+            <h6>
+              Size Large
+              <small>
+                {' '}
+                Add this class <code>.btn-lg</code>
+              </small>
+            </h6>
+            <p>
+              <CButton
+                size="lg"
+                className="btn-facebook btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-twitter btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-linkedin btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-flickr btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-tumblr btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-xing btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-github btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-stack-overflow btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-youtube btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-dribbble btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-instagram btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-pinterest btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-vk btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-yahoo btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-behance btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-reddit btn-brand mr-1 mb-1"
+              ></CButton>
+              <CButton
+                size="lg"
+                className="btn-vimeo btn-brand mr-1 mb-1"
+              ></CButton>
+            </p>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  );
+};
+
+export default BrandButtons;
